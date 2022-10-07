3f46130b199fd5b5de2f3b6333adc2728e811185
Esteve Fernandez <esteve@apache.org>
2022-10-07 15:51:48 +0200

    added missing files

---
+++ rmw_libp2p_cpp/src/ros_message_serialization.hpp
@@ -0,0 +1,34 @@
+// Copyright 2022 Esteve Fernandez All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef ROS_MESSAGE_SERIALIZATION_HPP_
+#define ROS_MESSAGE_SERIALIZATION_HPP_
+
+#include "rmw_libp2p_cpp/rmw_libp2p_rs.hpp"
+
+bool
+_serialize_ros_message(
+  const void * ros_message,
+  rs_libp2p_cdr_buffer * ser,
+  void * untyped_members,
+  const char * typesupport_identifier);
+
+bool
+_deserialize_ros_message(
+  rs_libp2p_cdr_buffer * buffer,
+  void * ros_message,
+  void * untyped_members,
+  const char * typesupport_identifier);
+
+#endif  // ROS_MESSAGE_SERIALIZATION_HPP_
